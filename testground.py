64af83c504e1cd60b5c4c9af4fef3438032b67d3
MichaelSagen <mikal.mikkelsen@hotmail.no>
2024-04-07 00:11:59 +0200

    gpio

--- testground.py
+++ testground.py
@@ -1,45 +1,19 @@
-"""
-FILE    : ADS_read.py
-AUTHOR  : Chandra.Wijaya
-VERSION : 1.2.0
-PURPOSE : read analog input
-
-test
-connect 1 potmeter 
-
-GND ---[   x   ]------ 3.3V
-           |
-
-measure at x (connect to AIN0).
-"""
-
-import os
+import RPi.GPIO as GPIO
 import time
-import ADS1x15
 
-# choose your sensor
-# ADS = ADS1x15.ADS1013(1, 0x48)
-# ADS = ADS1x15.ADS1014(1, 0x48)
-# ADS = ADS1x15.ADS1015(1, 0x48)
-# ADS = ADS1x15.ADS1113(1, 0x48)
-# ADS = ADS1x15.ADS1114(1, 0x48)
+# Set up GPIO
+GPIO.setmode(GPIO.BOARD)  # Sett GPIO-nummerering til fysisk nummer
+GPIO.setup(3, GPIO.OUT)  # Konfigurer pinne 3 som utgang
+GPIO.setup(5, GPIO.OUT)  # Konfigurer pinne 5 som utgang
 
-ADS = ADS1x15.ADS1115(1, 0x48)
+# Funksjon for å slå på pinne 3 og 5 i ett sekund asynkront
+def blink_pins():
+    GPIO.output(3, GPIO.HIGH)  # Slå på pinne 3
+    GPIO.output(5, GPIO.HIGH)  # Slå på pinne 5
+    time.sleep(1)  # Vent i ett sekund
+    GPIO.output(3, GPIO.LOW)  # Slå av pinne 3
+    GPIO.output(5, GPIO.LOW)  # Slå av pinne 5
 
-print(os.path.basename(__file__))
-print("ADS1X15_LIB_VERSION: {}".format(ADS1x15.__version__))
-
-# set gain to 4.096V max
-ADS.setGain(ADS.PGA_4_096V)
-f = ADS.toVoltage()
-
-while True :
-    val_0 = ADS.readADC(0)
-    val_1 = ADS.readADC(1)
-    val_2 = ADS.readADC(2)
-    val_3 = ADS.readADC(3)
-    print("Analog0: {0:d}\t{1:.3f} V".format(val_0, val_0 * f))
-    print("Analog1: {0:d}\t{1:.3f} V".format(val_1, val_1 * f))
-    print("Analog2: {0:d}\t{1:.3f} V".format(val_2, val_2 * f))
-    print("Analog3: {0:d}\t{1:.3f} V".format(val_3, val_3 * f))
-    time.sleep(1)
+# Kjør blink-funksjonen i en løkke for å fortsette å blinke pinsene
+while True:
+    blink_pins()
