71f3ac05191d4361203deb2bdb20d7599f587b85
MichaelSagen <mikal.mikkelsen@hotmail.no>
2024-04-07 00:23:00 +0200

    revers

--- testground.py
+++ testground.py
@@ -9,11 +9,11 @@ GPIO.setup(5, GPIO.OUT)  # Konfigurer pinne 5 som utgang
 # Funksjon for å slå på pinne 3 og 5 i ett sekund asynkront
 def blink_pins():
     GPIO.output(3, GPIO.HIGH)  # Slå på pinne 3
-    GPIO.output(5, GPIO.HIGH)  # Slå på pinne 5
+    GPIO.output(5, GPIO.LOW)  # Slå på pinne 5
     print ("blink på")
     time.sleep(0.1)  # Vent i ett sekund
     GPIO.output(3, GPIO.LOW)  # Slå av pinne 3
-    GPIO.output(5, GPIO.LOW)  # Slå av pinne 5
+    GPIO.output(5, GPIO.HIGH)  # Slå av pinne 5
     print ("blink av")
     time.sleep(0.1)  # Vent i ett sekund
 # Kjør blink-funksjonen i en løkke for å fortsette å blinke pinsene
