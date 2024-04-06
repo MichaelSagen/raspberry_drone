13c4a78f70fd5425c9c677b0549a22ab8dfe596c
MichaelSagen <mikal.mikkelsen@hotmail.no>
2024-04-06 23:10:00 +0200

    min

--- testground.py
+++ testground.py
@@ -1,18 +1,3 @@
-"""
-FILE    : ADS_read.py
-AUTHOR  : Chandra.Wijaya
-VERSION : 1.2.0
-PURPOSE : read analog input
-
-test
-connect 1 potmeter 
-
-GND ---[   x   ]------ 3.3V
-           |
-
-measure at x (connect to AIN0).
-"""
-
 import os
 import time
 import ADS1x15
@@ -24,22 +9,16 @@ import ADS1x15
 # ADS = ADS1x15.ADS1113(1, 0x48)
 # ADS = ADS1x15.ADS1114(1, 0x48)
 
-ADS = ADS1x15.ADS1115(1, 0xc8)
+ADS = ADS1x15.ADS1115(1, 0x48)
 
 print(os.path.basename(__file__))
 print("ADS1X15_LIB_VERSION: {}".format(ADS1x15.__version__))
 
 # set gain to 4.096V max
-ADS.setGain(ADS.PGA_0_256V)
-f = ADS.toVoltage()
+ADS.setGain(ADS.PGA_4_096V)
+print("Voltage")
 
 while True :
-    val_0 = ADS.readADC(0)
-    val_1 = ADS.readADC(1)
-    val_2 = ADS.readADC(2)
-    val_3 = ADS.readADC(3)
-    print("Analog0: {0:d}\t{1:.3f} V".format(val_0, val_0 * f))
-    print("Analog1: {0:d}\t{1:.3f} V".format(val_1, val_1 * f))
-    print("Analog2: {0:d}\t{1:.3f} V".format(val_2, val_2 * f))
-    print("Analog3: {0:d}\t{1:.3f} V".format(val_3, val_3 * f))
+    raw = ADS.readADC(0)
+    print("{0:.3f} V".format(ADS.toVoltage(raw)))
     time.sleep(1)
