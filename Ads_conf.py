f9c0bf0821708737f98eed0c4632ba0ed9e54543
MichaelSagen <mikal.mikkelsen@hotmail.no>
2024-04-19 22:22:34 +0200

    ras_conf

---
+++ Ads_conf.py
@@ -0,0 +1,33 @@
+import os
+import time
+import ADS1x15
+
+# choose your sensor
+# ADS = ADS1x15.ADS1013(1, 0x48)
+# ADS = ADS1x15.ADS1014(1, 0x48)
+# ADS = ADS1x15.ADS1015(1, 0x48)
+# ADS = ADS1x15.ADS1113(1, 0x48)
+# ADS = ADS1x15.ADS1114(1, 0x48)
+
+ADS = ADS1x15.ADS1115(1, 0x48)
+
+print(os.path.basename(__file__))
+print("ADS1X15_LIB_VERSION: {}".format(ADS1x15.__version__))
+
+# set gain to 4.096V max
+ADS.setGain(ADS.PGA_4_096V)
+f = ADS.toVoltage()
+ads_to_voltage = 0.000621
+
+while True :
+    val_0 = ADS.readADC(0)
+    val_1 = ADS.readADC(1)
+    val_2 = ADS.readADC(2)
+    val_3 = ADS.readADC(3)
+    time.sleep(4)
+    print("Analog0:", round(val_0*ads_to_voltage, 1),"V")
+    print("Analog1:", round(val_1*ads_to_voltage, 1),"V")
+    print("Analog2:", round(val_2*ads_to_voltage, 1),"V")
+    print("Analog3:", round(val_3*ads_to_voltage, 1),"V")
+    print("...................................")
+    print("...................................")
